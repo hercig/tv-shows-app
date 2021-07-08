7021227eea341cf35704c589baa67e34de3a5df5
Filip Hercig <fi.hercig@gmail.com>
2021-07-08 19:08:12 +0200

    Playing around with xcode

--- TV Shows/TV Shows/ViewController.swift
+++ TV Shows/TV Shows/ViewController.swift
@@ -9,11 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.titleLabel.text = "This is my second app"
     }
 
 
 }
-
