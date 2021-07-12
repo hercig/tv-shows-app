c69b1f24013a8a7b94d1e01c886f2b271a4ad5b1
Filip Hercig <fi.hercig@gmail.com>
2021-07-12 15:16:50 +0200

    Refactor - add fuction that shows and hides spinner for N seconds

--- TV Shows/TV Shows/Login/LoginViewController.swift
+++ TV Shows/TV Shows/Login/LoginViewController.swift
@@ -14,37 +14,33 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var spinner: UIActivityIndicatorView!
     
     var numberOfTaps = 0
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        buttonElement.layer.cornerRadius = 10
-        buttonElement.clipsToBounds = true
-        
-        let delay = 3
+    
+    func showSpinner(for delay: Int) -> Void {
+        self.buttonElement.isEnabled = false
         self.spinner.hidesWhenStopped = true
         self.spinner.startAnimating()
         DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(delay)) {
             self.spinner.stopAnimating()
+            self.buttonElement.isEnabled = true
         }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.showSpinner(for: 3)
+        
+        buttonElement.layer.cornerRadius = 10
+        buttonElement.clipsToBounds = true
         
     }
     
     @IBAction func buttonTap(_ sender: Any) {
         
-        self.buttonElement.isEnabled = false
-        self.spinner.hidesWhenStopped = true
-        let delay = 2
-        self.spinner.startAnimating()
+        self.showSpinner(for: 2)
         
         numberOfTaps += 1
         labelElement.text = "Number of taps: \(numberOfTaps)"
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(delay)) {
-            self.spinner.stopAnimating()
-            self.buttonElement.isEnabled = true
-        
-        }
+      
     }
 }
 
