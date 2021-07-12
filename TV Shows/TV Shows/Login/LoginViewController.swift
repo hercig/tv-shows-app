1a8a2025295eb70fe1889179416b6343fd3c8fe0
Filip Hercig <fi.hercig@gmail.com>
2021-07-12 15:11:38 +0200

    Add spinner (UIActivityView)

--- TV Shows/TV Shows/Login/LoginViewController.swift
+++ TV Shows/TV Shows/Login/LoginViewController.swift
@@ -11,6 +11,7 @@ class LoginViewController: UIViewController {
     
     @IBOutlet weak var labelElement: UILabel!
     @IBOutlet weak var buttonElement: UIButton!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
     
     var numberOfTaps = 0
 
@@ -20,12 +21,30 @@ class LoginViewController: UIViewController {
         buttonElement.layer.cornerRadius = 10
         buttonElement.clipsToBounds = true
         
+        let delay = 3
+        self.spinner.hidesWhenStopped = true
+        self.spinner.startAnimating()
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(delay)) {
+            self.spinner.stopAnimating()
+        }
+        
     }
     
     @IBAction func buttonTap(_ sender: Any) {
+        
+        self.buttonElement.isEnabled = false
+        self.spinner.hidesWhenStopped = true
+        let delay = 2
+        self.spinner.startAnimating()
+        
         numberOfTaps += 1
         labelElement.text = "Number of taps: \(numberOfTaps)"
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(delay)) {
+            self.spinner.stopAnimating()
+            self.buttonElement.isEnabled = true
+        
+        }
     }
-    
 }
 
